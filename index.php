a3c4371802b75c1140e1b24c500c3f5ae257440c
Sotero29 <soteroarmentagarcia05@gmail.com>
2023-10-07 01:08:37 -0600

    Ac2

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-
-
     require_once ("controladores/plantilla.controlador.php");
     require_once ("controladores/formularios.controlador.php");
     require_once ("modelos/formularios.modelos.php");
